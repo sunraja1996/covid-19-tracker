4c4f8939f77684e1a160202b2625dd99a3db8e1d
Shanmugaraja Gajendran <102361686+sunraja1996@users.noreply.github.com>
2023-08-13 23:16:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,35 @@ div.innerHTML = `<input type ="text" id="txt">
 <div id = "active"></div>
 <div id = "recovered"></div>
 <div id = "death"></div>`;
-div.style.textAlign="center";
+div.style.textAlign = "center";
 document.body.append(div);
 
+async function foo() {
+  let cname = document.getElementById("txt").value;
+  const url = `https://covid-19-tracking.p.rapidapi.com/v1/${cname}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '051943f206mshdd1fb136bdecdecp1bad3fjsn3095bacab206',
+      'X-RapidAPI-Host': 'covid-19-tracking.p.rapidapi.com'
+    }
+  };
 
-async function foo(){
-      let cname = document.getElementById("txt").value;
-      let res = await fetch(`https://covid-19-tracking.p.rapidapi.com/v1/${cname}`);
-      let res1 = await res.json();
-      console.log(res1);
+  try {
+    const response = await fetch(url, options);
+    const res1 = await response.json();
+    console.log(res1);
 
-      let index = res1.length-1;
-      var active = res1[index].Active;
-      var recovered = res1[index].Recovered;
-      var death = res1[index].Deaths;
-      console.log(active, recovered, death);
+    let index = res1.length - 1;
+    var active = res1[index].Active;
+    var recovered = res1[index].Recovered;
+    var death = res1[index].Deaths;
+    console.log(active, recovered, death);
 
-      document.getElementById("active").innerText = `Total Active Cases : ${active}`;
-      document.getElementById("recovered").innerText = `Total Recovered : ${recovered}`;
-      document.getElementById("death").innerText = `Total Deaths : ${death}`;
+    document.getElementById("active").innerText = `Total Active Cases : ${active}`;
+    document.getElementById("recovered").innerText = `Total Recovered : ${recovered}`;
+    document.getElementById("death").innerText = `Total Deaths : ${death}`;
+  } catch (error) {
+    console.error(error);
+  }
 }
